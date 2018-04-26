d8e8ac52f12cd527835d1311670df8c0824ed669
Jason Jones <jasonedwardjones@gmail.com>
2018-04-26 09:35:59 -0800

    wip

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,38 +3,72 @@
 </template>
 
 <script type="text/javascript">
+import axios from 'axios'
+
 export default {
   data () {
     return {
       maxNativeZoom: null,
       map: null,
       tileLayer: null,
+      selectedCells: null,
+      currentLayerUrl: null,
       layers: []
     }
   },
   mounted () {
     this.initMap()
     this.initLayers()
+    this.initMapEvents()
   },
   methods: {
     initMap () {
-      this.maxNativeZoom = 5
-
       this.map = L.map('map', {
         crs: L.CRS.Simple
       }).setView([-128, 128], 1)
 
-      this.tileLayer = L.tileLayer('http://localhost:8002/api/worlds/1/layers/3/tiles/{z}/{x}/{y}.png', {
-        maxZoom: 10,
-        maxNativeZoom: this.maxNativeZoom,
-        attribution: '',
-        noWrap: true,
-        crs: L.CRS.Simple
-      })
+      this.selectedCells = L.geoJSON(null, {
+        coordsToLatLng: nc => {
+          return this.map.unproject([nc[0], nc[1]], this.maxNativeZoom)
+        }
+      }).bindTooltip(layer => {
+        return layer.feature.properties.name
+      }).addTo(this.map)
 
-      this.tileLayer.addTo(this.map)
+      axios.get('https://cddamapapi.ralreegorganon.com/api/worlds/1').then(
+        response => {
+          console.log(response)
+
+          this.maxNativeZoom = response.data.maxz
+          this.currentLayerUrl = `https://cddamapapi.ralreegorganon.com/api/worlds/${response.data.id}/layers/${response.data.z['10'].layerId}`
+          this.tileLayer = L.tileLayer(`${this.currentLayerUrl}/tiles/{z}/{x}/{y}.png`, {
+            maxNativeZoom: this.maxNativeZoom,
+            attribution: '',
+            noWrap: true,
+            crs: L.CRS.Simple
+          })
+          this.tileLayer.addTo(this.map)
+        },
+        error => {
+          console.log(error)
+        }
+      )
     },
-    initLayers () {}
+    initLayers () {},
+    initMapEvents () {
+      this.map.on('click', e => {
+        console.log(this.maxNativeZoom)
+        const p = this.map.project(e.latlng, this.maxNativeZoom)
+        axios.get(`${this.currentLayerUrl}/cells/${p.x}/${p.y}`).then(
+          response => {
+            console.log(response)
+            this.selectedCells.clearLayers()
+            this.selectedCells.addData(response.data)
+          }, error => {
+            console.log(error)
+          })
+      })
+    }
   }
 }
 </script>
